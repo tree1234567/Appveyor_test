b37746112dc4505572659ece1610877f8bb9fdd4
STATS\aamador <aamador@stats.com>
2017-11-14 16:28:51 -0600

    fdsafds

--- pyappveyordemo/tests/test_extension.py
+++ pyappveyordemo/tests/test_extension.py
@@ -1,7 +1,45 @@
 from pyappveyordemo.extension import some_function
 from nose.tools import assert_equal
 import xmltodict
-from email_test import send_mail
+
+import smtplib
+from os.path import basename
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+
+
+def send_mail(send_from, send_to, subject, text, files=None,
+              server="webmail.stats.com"):
+    assert isinstance(send_to, list)
+
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = COMMASPACE.join(send_to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(text))
+
+    for f in files or []:
+        with open(f, "rb") as fil:
+            part = MIMEApplication(
+                fil.read(),
+                Name=basename(f)
+            )
+        # After the file is closed
+        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+        msg.attach(part)
+
+
+    smtp = smtplib.SMTP(server)
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.close()
+
+
+
+
 def test_some_function():
     send_mail("aamador@stats.com", ["aamador@stats.com"], "This is a test", "SOMETHING DUN FUCKED UP!", files=["test.txt"])
     assert_equal(some_function(0, 0), 2)
