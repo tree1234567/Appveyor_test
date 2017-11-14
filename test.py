dfd76c741592f207dbb0d18c1ef0f1698a097c60
STATS\aamador <aamador@stats.com>
2017-11-14 12:05:53 -0600

    fdsfadasfdasfdas

---
+++ test.py
@@ -0,0 +1,51 @@
+
+import sys
+
+
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+sys.stderr.write("This is an important message")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
