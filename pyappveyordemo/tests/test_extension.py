cf7e22dd7bcd850785494a363a3c6ce3a5934d5d
STATS\aamador <aamador@stats.com>
2017-11-14 16:25:09 -0600

    fdsafds

--- pyappveyordemo/tests/test_extension.py
+++ pyappveyordemo/tests/test_extension.py
@@ -1,8 +1,9 @@
 from pyappveyordemo.extension import some_function
 from nose.tools import assert_equal
 import xmltodict
-
+from email_test import send_mail
 def test_some_function():
+    send_mail("aamador@stats.com", ["aamador@stats.com"], "This is a test", "SOMETHING DUN FUCKED UP!", files=["test.txt"])
     assert_equal(some_function(0, 0), 2)
     assert_equal(some_function(0, 42), 0)
     assert_equal(some_function(41, 2), 1)
